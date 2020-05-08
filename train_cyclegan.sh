f04d4b6704e6760ff8a3e28c262c6fde67a22b6c
bohaohuang <bohao.huang@duke.edu>
2020-05-08 10:34:23 -0400

    auto checkout mrs

--- train_cyclegan.sh
+++ train_cyclegan.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+cd /data/users/bh163/code/mrs
+git checkout dev-cyclegan
+cd /data/users/bh163/code/pytorch-CycleGAN-and-pix2pix
 export PYTHONPATH=$PYTHONPATH:/data/users/bh163/code/mrs
 CUDA_VISIBLE_DEVICES=5 python train.py --dataroot ./datasets/maps --name syn2dg --model cycle_gan --direction AtoB --dataset_mode rs
+
+cd /data/users/bh163/code/mrs
+git checkout master
